f952795f4e2807812a9c0ab4296183b4dd3c6514
Gonzalo Hazel <ghnaitgam@gmail.com>
2022-11-13 00:28:54 -0600

    Incorporando Materialize

--- client/index.js
+++ client/index.js
@@ -1,5 +1,9 @@
 // Cargando Hoja de estilos
 import './stylesheet/style.css';
 import './stylesheet/mystyles.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
+
+M.AutoInit();
 
 console.log('Webpack Working!!! 📦 🦝');
