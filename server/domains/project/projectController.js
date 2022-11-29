26f03357d733fbee23e363ffa07f4e1592a63011
Gonzalo Hazel <ghnaitgam@gmail.com>
2022-11-28 22:47:08 -0600

    S04A03 🧪 Validacion

--- server/domains/project/projectController.js
+++ server/domains/project/projectController.js
@@ -8,12 +8,20 @@ const showAddForm = (req, res) => {
   res.render('project/add', viewModel);
 };
 
-// POST '/project/add'
-// POST '/project/create'
+// POST "/project/add"
+// POST "/project/create"
 const addProject = (req, res) => {
-  // extracyendo la informacion del formulario
-  const { Title, description } = req.body;
-  res.status(200).json({ Title, description });
+  // Desesctructurando y renombrando error de datos
+  const { errorData: error } = req;
+  // Verificando si hay error de validación
+  if (error) {
+    res.status(200).json(error);
+  } else {
+    // Desestructurando datos del formulario
+    const { validData: projectData } = req;
+    // Contestando los datos del proyecti
+    res.status(200).json(projectData);
+  }
 };
 
 export default { list, showAddForm, addProject };
--- server/domains/project/projectRouter.js
+++ server/domains/project/projectRouter.js
@@ -1,11 +1,24 @@
 import { Router } from 'express';
 import projectController from './projectController';
+// Importando factoria de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando objeto validador
+import projectValidator from './projectValidator';
+// Creando una isntancia del enrutador
 
 const router = new Router();
 
 router.get('/add', projectController.showAddForm);
 router.get('/', projectController.list);
-// POST '/project/add'
-router.post(['/add', '/create'], projectController.addProject);
+
+// POST "/project/add"
+router.post(
+  ['/add', '/create'],
+  ValidateFactory({
+    schema: projectValidator.projectSchema,
+    getObject: projectValidator.getProject,
+  }),
+  projectController.addProject
+);
 
 export default router;
